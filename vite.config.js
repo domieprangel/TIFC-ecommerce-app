18e52b2953788bf6faa9843d6ba015e4a2110f81
domieprangel <dominiquerangelperedo@gmail.com>
2025-09-05 14:12:20 -0600

    feat: se activa la página de register.html

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        products: resolve(__dirname, 'src/pages/products/products.html'),
+        products: resolve(__dirname, 'src/pages/products/products.html'), register: resolve(__dirname, 'src/pages/register/register.html')
         /*aboutUs: resolve(__dirname, 'src/pages/about-us/about-us.html'),
         contact: resolve(__dirname, 'src/pages/contact/contact.html'),
         register: resolve(__dirname, 'src/pages/register/register.html'),*/
